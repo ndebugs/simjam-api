b20860067614ac9d3284f6bb9009cce6cd660cbd
Yussin R. Hidayat <yussin.hidayat@gmail.com>
2021-08-06 20:13:22 +0700

    Update initial data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,7 +4,7 @@ INSERT INTO member (id, name, birth_date, address) VALUES (3, 'Joko Widodo', '19
 
 INSERT INTO transaction (id, member_id, type, amount, time) VALUES (1, 1, 'DEPOSIT', 1000000, '2020-08-17');
 INSERT INTO transaction (id, member_id, type, amount, time) VALUES (2, 2, 'DEPOSIT', 5000000, '2020-08-18');
-INSERT INTO transaction (id, member_id, type, amount, time) VALUES (3, 3, 'WITHDRAW', 2000000, '2020-09-30');
+INSERT INTO transaction (id, member_id, type, amount, time) VALUES (3, 3, 'WITHDRAW', -2000000, '2020-09-30');
 INSERT INTO transaction (id, member_id, type, amount, time) VALUES (4, 3, 'DEPOSIT', 1000000, '2020-11-10');
 INSERT INTO transaction (id, member_id, type, amount, time) VALUES (5, 1, 'DEPOSIT', 5000000, '2020-12-01');
-INSERT INTO transaction (id, member_id, type, amount, time) VALUES (6, 2, 'WITHDRAW', 2000000, '2020-12-01');
+INSERT INTO transaction (id, member_id, type, amount, time) VALUES (6, 2, 'WITHDRAW', -2000000, '2020-12-01');
